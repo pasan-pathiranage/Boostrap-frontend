fd89019a1242606858652f3d6672003f25b5249a
Pasan-Pathiranage <pasanpathiranage68@gmail.com>
2023-11-27 14:12:09 +0530

    fetch Products

--- ProductApp/js/index.js
+++ ProductApp/js/index.js
@@ -4,10 +4,32 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 const getProducts = () => {
-    fetch("http://localhost:8080/products").then ((response) =>{
-        console.log(response);
+    fetch("http://localhost:8080/products")
+        .then ((response) =>{ //if response is succcessful
+           return response.json();
+        }).then((data) =>{
 
-    }).catch((error) => {
-        console.log(error);
-    }); 
+            let products = "";
+            
+            data.map((product) => { 
+               
+                products += `<div  class="col-lg-3 col-sm-6 col-12">`; // start new column
+                products += `<div class = "card mb-4">`;
+                products += `<div class = "card-body">`;
+                products += `<h5 class="card-title">${product.name} <span class="badge bg-secondary">${product.category.name}</span></h5>`;
+                products += `<p class="card-text">Rs. ${product.price}</p>`;
+                products += `<a href="#" class="btn btn-primary">View Product</a>`;
+                products += `</div>`;
+                products += `</div>`;
+                products += `</div>`;
+
+            });
+            document.getElementById(`products`).innerHTML = products;
+
+            
+
+
+        }).catch((error) => {
+            console.log(error);
+        }); 
 }
